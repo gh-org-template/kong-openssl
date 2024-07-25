23ad5aad906592d8546de39281e8f9906f3fd8f8
Colin Hutchinson <chutchic@gmail.com>
2024-07-25 12:49:44 -0400

    feat(openssl): everything to build and test openssl

--- test.sh
+++ test.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-IFS=$'\n\t'
 
 if [ -n "${DEBUG:-}" ]; then
     set -x
 fi
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export $(grep -v '^#' $SCRIPT_DIR/.env)
+
 function test() {
-    ls -lah /tmp/build/out
+    echo '--- testing openssl ---'
+    cp -R /tmp/build/* /
+    mv /tmp/build /tmp/buffer # Check we didn't link dependencies to `/tmp/build/...`
+
+    /usr/local/kong/bin/openssl version
+    /usr/local/kong/bin/openssl version | grep -q $OPENSSL_VERSION
+    ldd /usr/local/kong/lib/libyaml.so
+
+    mv /tmp/buffer /tmp/build
+    echo '--- tested openssl ---'
 }
 
 test
