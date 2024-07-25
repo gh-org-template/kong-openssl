a496f6177c2861b39af9b14c28da7e38598a8ac7
Colin Hutchinson <hutchic@users.noreply.github.com>
2024-07-25 17:22:46 +0000

    feat(openssl): everything to build and test openssl (#3)

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,32 @@ ARG ARCHITECTURE=x86_64
 ARG DOCKER_REGISTRY=ghcr.io
 ARG DOCKER_IMAGE_NAME
 
+
 # List out all image permutations to trick dependabot
-FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-build-images:apk AS x86_64-linux-musl
-FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-build-images:rpm AS x86_64-linux-gnu
-FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-build-images:apk AS aarch64-linux-musl
-FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-build-images:rpm AS aarch64-linux-gnu
+FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-build-images:apk as x86_64-linux-musl
+RUN echo 'noop'
+
+FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-build-images:rpm as x86_64-linux-gnu
+RUN echo 'noop'
+
+FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-build-images:apk as aarch64-linux-musl
+RUN echo 'noop'
+
+FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-build-images:rpm as aarch64-linux-gnu
+RUN echo 'noop'
 
 # Run the build script
-FROM $ARCHITECTURE-$OSTYPE AS build
+FROM $ARCHITECTURE-$OSTYPE as build
 
 COPY . /src
 RUN /src/build.sh && /src/test.sh
 
+# Test scripts left where downstream images can run them
+COPY test.sh /test/kong-openssl/test.sh
+COPY .env /test/kong-openssl/.env
+
 
 # Copy the build result to scratch so we can export the result
-FROM scratch AS package
+FROM scratch as package
 
 COPY --from=build /tmp/build /
--- build.sh
+++ build.sh
@@ -1,14 +1,65 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-IFS=$'\n\t'
+set -eo pipefail
 
 if [ -n "${DEBUG:-}" ]; then
     set -x
 fi
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export $(grep -v '^#' $SCRIPT_DIR/.env | xargs)
+
 function main() {
-    rm -rf /tmp/build/* && uname -a >> /tmp/build/out
+    echo '--- installing openssl ---'
+    mkdir -p /tmp/build
+    with_backoff curl --fail -sSLo openssl.tar.gz "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz"
+    tar -xzvf openssl.tar.gz
+    pushd openssl-${OPENSSL_VERSION}
+        eval ./config \
+            -g shared \
+            -DPURIFY no-threads \
+            --prefix=/usr/local/kong \
+            --openssldir=/usr/local/kong no-unit-test '-Wl,-rpath,'\''$(LIBRPATH)'\'',--enable-new-dtags' && \
+        make -j2 && \
+        make install_sw DESTDIR=/tmp/build
+    popd
+    echo '--- installed openssl ---'
+}
+
+
+# Retries a command a configurable number of times with backoff.
+#
+# The retry count is given by ATTEMPTS (default 5), the initial backoff
+# timeout is given by TIMEOUT in seconds (default 1.)
+#
+# Successive backoffs double the timeout.
+function with_backoff {
+    local max_attempts=${ATTEMPTS-5}
+    local timeout=${TIMEOUT-5}
+    local attempt=1
+    local exitCode=0
+
+    while (( $attempt < $max_attempts ))
+    do
+        if "$@"
+        then
+            return 0
+        else
+            exitCode=$?
+        fi
+
+        echo "Failure! Retrying in $timeout.." 1>&2
+        sleep $timeout
+        attempt=$(( attempt + 1 ))
+        timeout=$(( timeout * 2 ))
+    done
+
+    if [[ $exitCode != 0 ]]
+    then
+        echo "You've failed me for the last time! ($@)" 1>&2
+    fi
+
+    return $exitCode
 }
 
 main
